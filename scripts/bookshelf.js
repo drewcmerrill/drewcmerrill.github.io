222f3c7330f79b328a67788a9a9e6cc836a2517e
Drew Merrill <drewcmerrill@gmail.com>
2023-08-01 13:00:42 -0700

    working on bookshelf

---
+++ scripts/bookshelf.js
@@ -0,0 +1,5 @@
+// for (let i = 0; i < 10; i++) {
+//   const cube = document.createElement("div");
+//   cube.classList.add("shelf");
+//   document.querySelector(".bookshelf").appendChild(cube);
+// }
